2525facb3f123fd5bcc252a40f3be1cbd2f08f02
Robthemop <1robinhess@gmail.com>
2018-11-05 19:10:40 +0100

    layout15

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,7 +11,7 @@ class BlogIndex extends React.Component {
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
 
     return (
-      <div style={{ background: '#FFF' }}>
+      <div style={{background: '#FFF'}}>
         <Helmet title={siteTitle} />
         <div style={{ textAlign: 'justify'}}>
         <h1>Impressum</h1>
