0fc9e0b624db4c5acaad26757755c9e509bc7a2d
Robthemop <1robinhess@gmail.com>
2018-11-06 12:58:42 +0100

    lel20

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -44,7 +44,7 @@ export const pageQuery = graphql`
       title
       publishDate(formatString: "MMMM Do, YYYY")
       heroImage {
-        sizes(maxWidth: 800, maxHeight: 1200) {
+        sizes(maxWidth: 3000, maxHeight: 2000, resizingBehavior: PAD) {
           ...GatsbyContentfulSizes_tracedSVG
         }
       }
