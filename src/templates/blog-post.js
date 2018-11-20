cf158547796b1c25977cf5dea02dd74a5b2be9bb
Robthemop <1robinhess@gmail.com>
2018-11-20 15:11:03 +0100

    cat 3.0

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -45,7 +45,7 @@ export const pageQuery = graphql`
       title
       publishDate(formatString: "MMMM Do, YYYY")
       category{
-        id: "title"
+        id: Title
       }
       heroImage {
         sizes(maxWidth: 1180, background: "rgb:000000") {
