d8d948abf498c9ad987538e4b62277881efbd946
Robthemop <1robinhess@gmail.com>
2018-11-20 12:55:38 +0100

    cat7

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -44,7 +44,7 @@ export const pageQuery = graphql`
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishDate(formatString: "MMMM Do, YYYY")
-      category{id: "title"}
+      category{id: title}
       heroImage {
         sizes(maxWidth: 1180, background: "rgb:000000") {
           ...GatsbyContentfulSizes_tracedSVG
