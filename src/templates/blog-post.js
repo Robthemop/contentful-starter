871585fe06fc01efe1d4dc912c21843ad0283ec9
Robthemop <1robinhess@gmail.com>
2018-11-20 13:52:12 +0100

    lets try it

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -45,7 +45,7 @@ export const pageQuery = graphql`
       title
       publishDate(formatString: "MMMM Do, YYYY")
       category{
-        title
+        title: category
       }
       heroImage {
         sizes(maxWidth: 1180, background: "rgb:000000") {
