6be1e95d9cf50568d9ba4005b9913dcd514ebac1
Robthemop <1robinhess@gmail.com>
2018-11-21 12:49:21 +0100

    checkboxcnt

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,8 @@ import Helmet from 'react-helmet'
 import styles from './blog.module.css'
 
 class Contact extends React.Component {
+
+
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
@@ -25,6 +27,14 @@ class Contact extends React.Component {
     <div><input name="name" placeholder="Your Name" type="text" /></div>
     <div style={{marginTop: 10}}><input name="email" placeholder="name@name.com" type="email" /></div>
     <div style={{marginTop: 10}}><textarea name="message" /></div>
+    <div style={{marginTop: 10}}><input
+            name="isGoing"
+            type="checkbox"
+             />
+             <label>
+             Ich habe die Datenschutzerklärung zur Kenntnis genommen. Ich stimme zu, dass meine Angaben und Daten zur Beantwortung meiner Anfrage elektronisch erhoben und gespeichert werden.
+             Hinweis: Sie können Ihre Einwilligung jederzeit für die Zukunft per E-Mail an 1robinhess@gmail.com widerrufen.
+<            /label></div>
     <div style={{marginTop: 10}}><button>Send</button></div>
   </form>
   <div style={{marginTop: 25}}>
