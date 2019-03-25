c92e546bde62286e1c816f5fdcecc1d02752030b
Robthemop <1robinhess@gmail.com>
2019-03-25 01:40:40 +0100

    thats it

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -11,7 +11,7 @@ class Impressum extends React.Component {
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
 
     return (
-        <div style={{background: '#FFF'}}>
+        <div style={{background: '#fff'}}>
         <Helmet title={siteTitle} />
         <div style={{textAlign: 'justify'}}>
         <h1>Impressum</h1>
