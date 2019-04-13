f8920b3b1509d379ed65dbef00b0e2b573101a3e
Robthemop <1robinhess@gmail.com>
2019-04-14 00:23:56 +0200

    Beautify2

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,21 +2,39 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styles from './navigation.module.css'
 
+
 export default () => (
+
     <nav role="navigation">
         <ul className={styles.navigation}>
 
             <li className={styles.navigationItem}>
-                <Link to="/">Blog</Link>
+                <Link exact to="/"
+                      activeStyle={{ color: "red"}}
+                      partiallyActive={true}
+                >
+                    <b>Blog</b>
+                </Link>
             </li>
 
             <li className={styles.navigationItem}>
-                <Link to="/contact/">Kontakt</Link>
+                <Link to="/contact/"
+                      activeStyle={{ color: "red" }}
+                      partiallyActive={true}
+                >
+                    <b>Kontakt</b>
+                </Link>
             </li>
 
             <li className={styles.navigationItem}>
-                <Link to="/impressum/">Impressum</Link>
+                <Link to="/impressum/"
+                      activeStyle={{ color: "red" }}
+                      partiallyActive={true}
+                >
+                    <b>Impressum</b>
+                </Link>
             </li>
+
         </ul>
     </nav>
 )
