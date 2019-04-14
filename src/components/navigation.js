bf0edb0511b1c738f83056c176dcf7380b09dfac
Robthemop <1robinhess@gmail.com>
2019-04-14 14:48:39 +0200

    Buttons nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styles from './navigation.module.css'
+import {css, cx} from 'emotion'
+
+const color = '#BF4B4B'
 
 
 export default () => (
@@ -8,27 +11,64 @@ export default () => (
     <nav role="navigation">
         <ul className={styles.navigation}>
 
-            <li className={styles.navigationItem}>
+            <li className={css`
+                color: "#026873";
+                display: inline-flex;
+                align-items: center;
+                margin: 0 1em;
+                padding: 32px;
+                background-color: #03A6A6;
+                font-size: 24px;
+                border-radius: 4px;
+                &:hover {
+                color: ${color};
+            }
+                `}
+            >
                 <Link exact to="/"
-                      activeStyle={{ color: "red"}}
+                      activeStyle={{color: "#FF665A"}}
                       partiallyActive={true}
                 >
                     <b>Blog</b>
                 </Link>
             </li>
 
-            <li className={styles.navigationItem}>
+            <li className={css`
+                color: "#026873";
+                display: inline-flex;
+                align-items: center;
+                margin: 0 1em;
+                padding: 32px;
+                background-color: #03A6A6;
+                font-size: 24px;
+                border-radius: 4px;
+                &:hover {
+                color: ${color};
+            }
+                `}>
                 <Link to="/contact/"
-                      activeStyle={{ color: "red" }}
-                      partiallyActive={true}
+                      activeStyle={{color: "#FF665A"}}
                 >
                     <b>Kontakt</b>
                 </Link>
             </li>
 
-            <li className={styles.navigationItem}>
+            <li className={css`
+                color: "#026873";
+                display: inline-flex;
+                align-items: center;
+                margin: 0 1em;
+                padding: 32px;
+                background-color: #03A6A6;
+                font-size: 24px;
+                border-radius: 4px;
+                &:hover {
+                color: ${color};
+            }
+                `}>
+
                 <Link to="/impressum/"
-                      activeStyle={{ color: "red" }}
+                      activeStyle={{color: "#FF665A"}}
                       partiallyActive={true}
                 >
                     <b>Impressum</b>
