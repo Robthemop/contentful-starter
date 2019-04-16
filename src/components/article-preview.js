14b7bde2f970bc6953ea254ba3ae9c513e11d2fa
Robthemop <1robinhess@gmail.com>
2019-04-16 19:58:56 +0200

    Alle Bilder sind gleich du hueenen

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -7,23 +7,22 @@ import styles from './article-preview.module.css'
 export default ({article}) => (
     <div className={styles.preview}>
 
-        <h3 className={styles.previewTitle}>
-            <Link to={`/blog/${article.slug}`}
-                  style={{textDecoration: 'none'}}>
-                {article.title}
-            </Link>
-        </h3>
-
         <Link to={`/blog/${article.slug}`} style={{textDecoration: 'none'}}>
             <Img alt="" sizes={article.heroImage.sizes}></Img>
         </Link>
-
         <small>
             <Link to={`/blog/${article.slug}`}
                   style={{textDecoration: 'none'}}>
                 {article.publishDate}
             </Link>
         </small>
+
+        <h3 className={styles.previewTitle}>
+            <Link to={`/blog/${article.slug}`}
+                  style={{textDecoration: 'none'}}>
+                {article.title}
+            </Link>
+        </h3>
 
         <Link to={`/blog/${article.slug}`}
               style={{textDecoration: 'none'}}>
