1daf2a431cc3671586375f2249b1831bbd1455d9
Robthemop <1robinhess@gmail.com>
2019-05-19 21:28:40 +0200

    Neues Content Model - articles ist implementiert und alle blog posts wurden als article geschrieben.

---
+++ src/pages/Boulder-Test.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+import Hero from '../components/hero'
+import ArticlePreview from '../components/article-preview'
+import Navigation from '../components/navigation'
+import Link from "gatsby-link";
+
+
+class BoulderTest extends React.Component {
+    render() {
+        const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+        const articles = get(this, 'props.data.allContentfulArticle.edges')
+        const [author] = get(this, 'props.data.allContentfulPerson.edges')
+
+        return (
+            <div style={{background: '#fff'}}>
+                <Helmet title={siteTitle}/>
+
+                <Hero data={author.node}/>
+
+
+                <div className="wrapper">
+
+                    <Navigation/>
+
+                    <Link to="/kategorien/"
+                          style={{textDecoration: 'none'}}>
+                        <h2 className="section-headline">Alle Kategorien anzeigen</h2>
+                    </Link>
+
+                    <ul className="article-list">
+                        {articles.map(({node}) => {
+                            return (
+                                <li key={node.slug}>
+                                    <ArticlePreview article={node}/>
+                                </li>
+                            )
+                        })}
+                    </ul>
+
+                    <Link to="/kategorien/"
+                          style={{textDecoration: 'none'}}>
+                        <h2 className="section-bottomLine">Alle Kategorien anzeigen</h2>
+                    </Link>
+
+                    <div>
+
+                        <a href="https://www.bergfreunde.de/?pid=16093&_$ja=tsid:52154"
+                           title="Ausrüstung für Klettern, Bergsport und Outdoor bei Bergfreunde.de kaufen"
+                           rel="nofollow"
+                           target="_blank">
+                            <img src="https://www.bergfreunde-partner.de/banner/DE/DE_AF_Banner_728x90.jpg"
+                                 alt="Ausrüstung für Klettern, Bergsport und Outdoor bei Bergfreunde.de kaufen"
+                                 border="0"/></a>
+                        <img src="https://partner.bergfreunde.de/go.cgi?pid=16093&wmid=30&cpid=1&prid=1&subid=&view=1"
+                             height="1"
+                             width="1"
+                             border="0"/>
+
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BoulderTest
+
+export const pageQuery = graphql`
+  query BoulderTest {
+  allContentfulArticle(sort: { fields: [publishDate], order: DESC }) {
+      edges {
+        node {
+          title
+          slug
+          publishDate(formatString: "MMMM Do, YYYY")
+          
+          heroImage {
+            sizes(maxWidth: 400, maxHeight: 400, resizingBehavior: FILL) {
+             ...GatsbyContentfulSizes_tracedSVG
+            }
+          }
+          
+          description {
+            childMarkdownRemark {
+              html
+            }
+          }
+          
+        }
+      }
+    }
+    allContentfulPerson(filter: { id: { eq: "c15jwOBqpxqSAOy2eOO4S0m" } }) {
+      edges {
+        node {
+          name
+          heroImage: image {
+            sizes(
+              maxWidth: 3728
+              maxHeight: 2376
+              resizingBehavior: PAD
+              background: "rgb:FFFFFF"
+            ) {
+              ...GatsbyContentfulSizes_tracedSVG
+            }
+          }
+        }
+      }
+    }
+    allContentfulCategory(filter:{ contentful_id: {eq: "5Yqtk99s2c0YgC8QsMceGc"}}){
+    edges{
+      node{
+        title
+        }
+    }
+  }
+  }
+`
---
+++ src/templates/article.js
@@ -0,0 +1,124 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import get from 'lodash/get'
+import Img from 'gatsby-image'
+import Navigation from '../components/navigation'
+import pictureStyles from '../components/picture.module.css'
+import Link from "gatsby-link";
+import heroStyles from '../components/hero.module.css'
+
+
+class ArticleTemplate extends React.Component {
+    render() {
+        const article = get(this.props, 'data.contentfulArticle')
+        const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+
+        return (
+            <div style={{background: '#FFF'}}>
+
+                <div className="wrapper">
+
+                    <Navigation/>
+
+                    <div>
+                        <Link to={`/${article.category.title}/`}
+                              style={{textDecoration: 'none'}}>
+                            <h2 className="section-headline">{article.category.title}</h2>
+                        </Link>
+                    </div>
+
+                    <Helmet title={`${article.title} | ${article.category.title}`}/>
+
+                    <h2>{article.title}</h2>
+
+
+                    <div className={pictureStyles.p}
+                         dangerouslySetInnerHTML={{
+                             __html: article.body.childMarkdownRemark.html,
+                         }}
+                    />
+
+                    <div className={pictureStyles.picture}>
+                        <Img className={pictureStyles.pictureImg} alt={article.title} sizes={article.pictureOne.sizes}/>
+                    </div>
+
+                    <div className={pictureStyles.p}
+                         dangerouslySetInnerHTML={{
+                             __html: article.bodyTwo.childMarkdownRemark.html,
+                         }}
+                    />
+
+                    <div className={pictureStyles.picture}>
+                        <Img className={pictureStyles.pictureImg} alt={article.title} sizes={article.pictureTwo.sizes}/>
+                    </div>
+
+                    <div className={pictureStyles.p}
+                         dangerouslySetInnerHTML={{
+                             __html: article.bodyThree.childMarkdownRemark.html,
+                         }}
+                    />
+
+                    <Link to="/kategorien/"
+                          style={{textDecoration: 'none'}}>
+                        <h2 className="section-bottomLine">Alle Kategorien anzeigen</h2>
+                    </Link>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ArticleTemplate
+
+export const pageQuery = graphql`
+  query ArticleBySlug($slug: String!) {
+    contentfulArticle(slug: { eq: $slug }) {
+    
+      title
+      publishDate(formatString: "MMMM Do, YYYY")
+      
+      category{
+        title
+      }
+      
+      heroImage {
+        sizes(maxWidth: 1180, background: "rgb:255255") {
+          ...GatsbyContentfulSizes_tracedSVG
+        }
+      }
+
+      
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+      
+      pictureOne {
+        sizes(maxWidth: 4000, background: "rgb:255255") {
+          ...GatsbyContentfulSizes_tracedSVG
+        }
+      }
+      
+      bodyTwo {
+        childMarkdownRemark {
+          html
+        }
+      }
+      
+      pictureTwo {
+        sizes(maxWidth: 4000, background: "rgb:255255") {
+          ...GatsbyContentfulSizes_tracedSVG
+        }
+      }
+      
+      bodyThree {
+        childMarkdownRemark {
+          html
+        }
+      }
+      
+    }
+  }
+`
