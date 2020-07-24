5506b0d888123da057ae00188482bdc4e36e4315
Robthemop <1robinhess@gmail.com>
2020-07-24 22:13:03 +0200

    black and white design 4.0

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,7 +7,7 @@ export default () => (
         <div className={styles.navigation}>
             <div className={styles.links}>
                 <Link to="/">
-                    <h2 className="navigation-home-title">boulderboys</h2>
+                    <h2 className="navigation-home-title">Boulder Boys</h2>
                 </Link>
             </div>
         </div>
