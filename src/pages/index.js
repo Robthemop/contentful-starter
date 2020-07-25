9ca59842fe4567c8135a11424aff09f5b6f081b6
Robthemop <1robinhess@gmail.com>
2020-07-25 14:52:48 +0200

    bergfreunde banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,6 +51,28 @@ class RootIndex extends React.Component {
                     <SocialIcon url="https://www.facebook.com/boulderobin/" bgColor="#000" style={{ height: 35, width: 35 }}/>
                 </div>
 
+                <a className="banner-image-desktop"
+                    href="http://www.bergfreunde.de/#Q1322C16093"
+                    rel="nofollow"
+                    title="SummerSale 2020 970x250">
+                    <img src="https://partner.bergfreunde.de/go.cgi?pid=16093&wmid=1322&cpid=1&prid=1&subid=&view=1&target=https://www.bergfreunde-partner.de/banner/DE/SummerSale2020/DE_SummerSale_2020_970x250.jpg"
+                         height="250"
+                         width="970"
+                         border="0"
+                         alt="SummerSale 2020 970x250" />
+                </a>
+
+                <a className="banner-image-mobile"
+                    href="http://www.bergfreunde.de/#Q1327C16093"
+                   rel="nofollow"
+                   title="SummerSale 2020 200x200 1">
+                    <img src="https://partner.bergfreunde.de/go.cgi?pid=16093&wmid=1327&cpid=1&prid=1&subid=&view=1&target=https://www.bergfreunde-partner.de/banner/DE/SummerSale2020/DE_SummerSale_2020_200x200_1.jpg"
+                         height="200"
+                         width="200"
+                         border="0"
+                         alt="SummerSale 2020 200x200 1" />
+                </a>
+
                 <div className="wrapper--index">
 
                     <h2>Kategorien</h2>
